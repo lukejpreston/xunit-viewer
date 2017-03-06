d40b38593b8bf3bb19080d2b014cfea370bc080e
Luke Preston <luke.preston@bbc.co.uk>
2017-03-06 07:30:43 +0000

    adding an assertion

--- parser/src/index.spec.js
+++ parser/src/index.spec.js
@@ -2,8 +2,11 @@ const fs = require('fs')
 const path = require('path')
 const parser = require('./')
 
+const data = path.resolve(__dirname, '../../data')
+const output = require(path.join(data, 'output.json'))
+
 const getInput = (name) => {
-    return fs.readFileSync(path.resolve(__dirname, `../../data/${name}.xml`)).toString()
+    return fs.readFileSync(path.join(data, `${name}.xml`)).toString()
 }
 
 const logSuites = (suites) => {
@@ -13,8 +16,9 @@ const logSuites = (suites) => {
 describe('parser', () => {
     it('works', () =>{
         let input = getInput('complete_multi_suites')
-        let output = parser.parse(input)
 
-        logSuites(output)
+        let parsed = parser.parse(input)
+
+        expect(parsed).toEqual(output)
     })
 })
