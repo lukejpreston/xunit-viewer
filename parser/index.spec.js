dacecb2c1f1526df844eb5f224faa4bc0de773c3
Luke Preston <luke.preston@bbc.co.uk>
2017-03-06 17:42:30 +0000

    can now run tests

--- parser/index.spec.js
+++ parser/index.spec.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const parser = require('./')
 
-const data = path.resolve(__dirname, '../../data')
+const data = path.resolve(__dirname, '../data')
 const output = require(path.join(data, 'output.json'))
 
 const getInput = (name) => {
