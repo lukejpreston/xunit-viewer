f15a2c9e31c11416ac35dc34399ebc1fea1e3182
Luke Preston <luke.preston@bbc.co.uk>
2017-03-10 17:57:04 +0000

    does the awesome reload stuff

--- cli/start-server.js
+++ cli/start-server.js
@@ -21,17 +21,17 @@ module.exports = (options) => {
         })
     })
   } else {
+    let send = 'LOADING... <script>setTimeout(function () { window.location.reload() }, 500)</script>'
+    app.get('/', (req, res) => {
+      res.send(send)
+    })
     render(options)
       .then(result => {
-        app.get('/', (req, res) => {
-          res.send(result)
-        })
+        send = result
       })
       .catch(err => {
-        app.get('/', (req, res) => {
-          if (err.file) res.send(`<span>File: ${err.file}</span><pre>${err.stack}</pre>`)
-          else res.send(`<pre>${err.stack}</pre>`)
-        })
+        if (err.file) send = `<span>File: ${err.file}</span><pre>${err.stack}</pre>`
+        else send = `<pre>${err.stack}</pre>`
       })
   }
 
