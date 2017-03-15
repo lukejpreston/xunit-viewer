c091a8e66fa20c575bebb393e9271ca7b117b1b1
Luke Preston <luke.preston@bbc.co.uk>
2017-03-15 17:48:06 +0000

    pre populate with suites instead of waiting for sockets

--- cli/resolve-argv.js
+++ cli/resolve-argv.js
@@ -1,30 +1,65 @@
-module.exports = () => {
-  let options = {}
+  const map = {
+    passed: 'pass',
+    passing: 'pass',
+    pass: 'pass',
+    failure: 'fail',
+    failed: 'fail',
+    failint: 'fail',
+    fail: 'fail',
+    errored: 'error',
+    erroring: 'error',
+    error: 'error',
+    skipped: 'skip',
+    skipping: 'skip',
+    skip: 'skip',
+    all: 'all'
+  }
 
-  process.argv.forEach(arg => {
-    if (arg.includes('--results')) options.results = arg.replace('--results=', '')
-    else if (arg.includes('--ignore')) options.ignore = arg.replace('--ignore=', '').split(',')
-    else if (arg.includes('--output')) options.output = arg.replace('--output=', '')
-    else if (arg.includes('--title')) options.title = arg.replace('--title=', '')
-    else if (arg.includes('--port')) options.port = parseInt(arg.replace('--port=', ''), 10)
-    else if (arg.includes('--color')) options.color = arg.replace('--color=', '')
-    else if (arg.includes('--watch')) options.watch = arg.replace('--watch=', '').replace('--watch', '')
-    else if (arg.includes('--dev')) options.dev = arg.replace('--dev=', '').replace('--dev', '')
-    else if (arg.includes('--terminal')) options.terminal = arg.replace('--terminal=', '').replace('--terminal', '')
+  module.exports = () => {
+    let options = {
+      filter: {}
+    }
 
-// --filter.suites.value="Suite names matching this value"
-// --filter.suites.types=all,pass,fail,skip,error,unknown
-// --filter.tests.value="Test names matching this value"
-// --filter.tests.types=all,pass,fail,skip,error,unknown
-// --filter.properties.value="Properties matching with key or value matching this value"
-// --filter.properties.types=all
-  })
+    process.argv.forEach(arg => {
+      if (arg.includes('--results')) options.results = arg.replace('--results=', '')
+      else if (arg.includes('--ignore')) options.ignore = arg.replace('--ignore=', '').split(',')
+      else if (arg.includes('--output')) options.output = arg.replace('--output=', '')
+      else if (arg.includes('--title')) options.title = arg.replace('--title=', '')
+      else if (arg.includes('--port')) options.port = parseInt(arg.replace('--port=', ''), 10)
+      else if (arg.includes('--color')) options.color = arg.replace('--color=', '')
+      else if (arg.includes('--watch')) options.watch = arg.replace('--watch=', '').replace('--watch', '')
+      else if (arg.includes('--dev')) options.dev = arg.replace('--dev=', '').replace('--dev', '')
+      else if (arg.includes('--terminal')) options.terminal = arg.replace('--terminal=', '').replace('--terminal', '')
 
-  if (options.dev === '' || options.dev === 'true') options.dev = true
-  else options.dev = false
+      if (arg.includes('--filter')) {
+        arg = arg.replace('--filter', '')
+        let split = arg.split('=')
 
-  if (options.watch === '' || options.dev === 'true') options.watch = true
-  else options.watch = false
+        let keys = split[0].split('.')
+        let x = keys[0]
+        let thing = keys[1]
+        let filter = keys[2]
+        let value = split[1].split(',')
+        value.map(v => {
+          if (!map.hasOwnProperty(v)) return 'unknown'
+          return map[v]
+        })
+        console.log(thing, filter, value)
+      }
 
-  return options
-}
+    // if (arg.includes('--filter.suites.search'))
+    // if (arg.includes('--filter.suites.hidden'))
+    // if (arg.includes('--filter.tests.search'))
+    // if (arg.includes('--filter.tests.hidden'))
+    // if (arg.includes('--filter.properties.search'))
+    // if (arg.includes('--filter.properties.hidden'))
+    })
+
+    if (options.dev === '' || options.dev === 'true') options.dev = true
+    else options.dev = false
+
+    if (options.watch === '' || options.dev === 'true') options.watch = true
+    else options.watch = false
+
+    return options
+  }
