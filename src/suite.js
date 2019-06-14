450e90d239a17d757bd9d2fa97d6557561d67049
Luke Preston <lukep@ceh.ac.uk>
2019-06-14 09:15:13 +0100

    adding count on suites

--- src/suite.js
+++ src/suite.js
@@ -65,28 +65,69 @@ const Test = ({ messages, status, time, name, active = false }) => {
   </div>
 }
 
+const SuiteCount = ({ count, type }) => <span className='suite-count'>
+  <span className='icon'>
+    <i className={`fas fa-${icons[type]}`} aria-hidden='true' />
+  </span>
+  {count}
+</span>
+
 const Suite = ({ name, active = false, properties = {}, time, tests = {} }) => {
+  let passed = 0
+  let failure = 0
+  let skipped = 0
+  let error = 0
+  let unknown = 0
+  Object.keys(tests).forEach(key => {
+    const status = tests[key].status
+    if (status === 'passed') passed += 1
+    else if (status === 'failure') failure += 1
+    else if (status === 'skipped') skipped += 1
+    else if (status === 'error') error += 1
+    else unknown += 1
+  })
+
   const hasTests = Object.keys(tests).length > 0
   const hasProperties = Object.keys(properties).length > 0
   const containsSomething = hasTests || hasProperties
-  const [open, setOpen] = useState(!containsSomething)
+  const [open, setOpen] = useState(active)
   return <div className={`card suite is-${open ? 'active' : 'inactive'} is-${containsSomething ? 'populated' : 'empty'}`}>
     <button className='card-header' onClick={() => { if (containsSomething) setOpen(!open) }} disabled={!containsSomething}>
       <p className='card-header-title'>
         <span>{title(name)}</span>
-        {time ? <small>{time}</small> : null}
+        {time ? <small>time = {time}</small> : null}
+
       </p>
+
       {containsSomething ? <span className='card-header-icon'>
         <span className='icon'>
           <i className='fas fa-angle-down' />
         </span>
       </span> : null}
+      {containsSomething ? <p className='suite-count-container'>
+        <SuiteCount type='passed' count={passed} />
+        <SuiteCount type='failure' count={failure} />
+        <SuiteCount type='error' count={error} />
+        <SuiteCount type='skipped' count={skipped} />
+        <SuiteCount type='unknown' count={unknown} />
+      </p> : null}
     </button>
     {!open && containsSomething ? <div className='card-content'>
       <div className='content'>
         {hasProperties ? <Properties properties={properties} /> : null}
         <div>
-          {Object.keys(tests).map(key => <Test key={key} {...tests[key]} />)}
+          {Object.keys(tests)
+            // .sort((left, right) => {
+            //   const leftStatus = tests[left].status || 'unknown'
+            //   const rightStatus = tests[right].status || 'unknown'
+            //   if (leftStatus === 'failure' || rightStatus === 'failure') return -5
+            //   if (leftStatus === 'error' || rightStatus === 'error') return -4
+            //   if (leftStatus === 'unknown' || rightStatus === 'unknown') return -3
+            //   if (leftStatus === 'passed' || rightStatus === 'passed') return -2
+            //   if (leftStatus === 'skipped' || rightStatus === 'skipped') return -1
+            //   return 0
+            // })
+            .map(key => <Test key={key} {...tests[key]} />)}
         </div>
       </div>
     </div> : null}
