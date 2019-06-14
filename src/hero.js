7ec312cac16607d07e81869d8ad2ce6782c99a93
Luke Preston <lukep@ceh.ac.uk>
2019-06-14 11:05:03 +0100

    making hero work on mobile

--- src/hero.js
+++ src/hero.js
@@ -5,7 +5,7 @@ import { Slider } from 'react-burgers'
 export default ({ active, onClick }) => <section className='hero is-black'>
 
   <div className='container'>
-    <div className='columns'>
+    <div className='columns is-mobile'>
       <div className='column is-1'>
         <Slider className='button' active={active} onClick={onClick} borderRadius={4} color='#fff' />
       </div>
