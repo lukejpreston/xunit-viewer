e434c7ec522cddc997ab41a945fa1bdc4bacfe26
Luke Preston <lukep@ceh.ac.uk>
2019-11-29 14:23:01 +0000

    tested $CURRENT

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -e
+
 npm run lint
 npm run release
 npm run test:ci
-
 CURRENT=$(echo npm version | grep xunit-viewer | cut -d"'" -f4)
+git commit -am 'tested $CURRENT'
+
 npm version ${1-patch}
 npm publish
 LATEST=$(echo npm version | grep xunit-viewer | cut -d"'" -f4)
