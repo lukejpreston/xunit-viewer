92a2332599366b3e6925ce64882b7cd731a88b69
Luke Preston <luke.preston@trailapp.com>
2021-04-14 09:31:47 +0100

    update junit, fix server and save bug

--- xunit-viewer.js
+++ xunit-viewer.js
@@ -31,7 +31,7 @@ module.exports = async (args) => {
       const outputFile = path.resolve(process.cwd(), args.output)
       fs.writeFileSync(outputFile, result)
       console.log('Written to:', logger.file(outputFile))
-      process.exit(0)
+      if (!args.server) process.exit(0)
     }
   }
 
