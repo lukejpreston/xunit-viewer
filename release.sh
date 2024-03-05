ce44c145c6d727fab89d5e43e3e3d43e667f39dd
Luke Preston <luke.preston@theaccessgroup.com>
2024-03-05 09:07:58 +0000

    tested 10.6.0

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ npm run build:cli
 npm run test:ci
 ./bin/xunit-viewer.js -r junit.xml -c -C false
 CURRENT=$(echo $(npm version | grep xunit-viewer | cut -d"'" -f4))
-git commit -am "tested $CURRENT"
+git commit --allow-empty -am "tested $CURRENT"
 
 npm version ${1-patch}
 npm publish
@@ -16,7 +16,7 @@ LATEST=$(echo npm version | grep xunit-viewer | cut -d"'" -f4)
 npm run release:demo
 
 git add -A
-git commit -am "release demo $LATEST"
+git commit --allow-empty -am "release demo $LATEST"
 
 git push
 git push --tags
